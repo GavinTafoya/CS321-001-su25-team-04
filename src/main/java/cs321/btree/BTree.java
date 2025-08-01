c4be744f942ecf1f9646b2e48e1269b4c8375237
Ahmad Rao <ahmadrao@u.boisestate.edu>
2025-08-01 13:46:26 -0600

    Insert Method #4
    Insert method completed and passes 9 of 17 tests

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -2,9 +2,38 @@ package cs321.btree;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BTree<T extends Comparable<T>> implements BTreeInterface
 {
+
+    /**
+     * Returns all keys currently stored in the B-tree in a increasing order
+     *
+     * @return a non-null array of keys in sorted order and the array is empty if the tree has no keys
+     */
+    public String[] getSortedKeyArray() {
+        ArrayList<String> out = new ArrayList<>();
+        inOrder(root, out);
+        return out.toArray(new String[0]);
+    }
+
+
+    /**
+     * Performs a traversal in order starting at node and appends each keys String value to output
+     *
+     * @param node    the node to traverse
+     * @param output  accumulator list that receives the keys encountered during traversal
+     */
+    private void inOrder(Node<T> node, ArrayList<String> output) {
+        if (node == null) return;
+        for (int i = 0; i < node.numKeys; i++) {
+            if (!node.isLeaf) inOrder(node.childPointers[i], output);
+            output.add(node.keys[i].getKey());
+        }
+        if (!node.isLeaf) inOrder(node.childPointers[node.numKeys], output);
+    }
     //------------------------------------------------------------------
     // Private Node Class
     //------------------------------------------------------------------
@@ -17,6 +46,9 @@ public class BTree<T extends Comparable<T>> implements BTreeInterface
         private Node<S> left,right,parent;
         private boolean isLeaf;
         private S key;
+        int numKeys;
+        TreeObject[] keys;
+        Node<S>[] childPointers;
 
         /**
          * Constructor for a node.
@@ -25,6 +57,12 @@ public class BTree<T extends Comparable<T>> implements BTreeInterface
         public Node(S element) {
             key = element;
             left = right = parent = null;
+            keys = new TreeObject[2 * degree - 1];
+            @SuppressWarnings("unchecked")
+            Node<S>[] kids = (Node<S>[]) new Node[2 * degree];
+            childPointers = kids;
+            numKeys = 0;
+            isLeaf  = true;
         }
 
 
@@ -58,8 +96,10 @@ public class BTree<T extends Comparable<T>> implements BTreeInterface
      * Creates an empty BTree
      */
     public BTree(String name){
-        this.root  = null;
+        this.degree = 2;
+        this.root  = new Node<>(null);
         this.size = 0;
+        this.height = 0;
     }
 
     public BTree(int degree, String name) {
@@ -116,7 +156,6 @@ public class BTree<T extends Comparable<T>> implements BTreeInterface
         return height; // This is wrong
     }
 
-
     /**
      * Insert a given SSH key into the B-Tree. If the key already exists in the B-Tree,
      * the frequency count is incremented. Otherwise, a new node is inserted
@@ -124,11 +163,94 @@ public class BTree<T extends Comparable<T>> implements BTreeInterface
      *
      * @param obj A TreeObject representing an SSH key.
      */
-    // test push
-
     @Override
     public void insert(TreeObject obj) throws IOException {
+        if (root == null) {
+            root = new Node<>(null);
+            root.keys[0] = obj;
+            root.numKeys = 1;
+            size = 1;
+            height = 0;
+            return;
+        }
+        Node<T> cursor = root;
+        while (true) {
+            int position = Arrays.binarySearch(cursor.keys,0, cursor.numKeys, obj);
+            if (position >= 0) {
+                cursor.keys[position].incCount();
+                return;
+            }
+            position = -position - 1;
+            if (cursor.isLeaf){
+                break;
+            }
+            if (cursor.childPointers[position].numKeys == 2 * degree - 1) {
+                splitChild(cursor, position);
+                if (obj.compareTo(cursor.keys[position]) > 0) position++;
+            }
+            cursor = cursor.childPointers[position];
+        }
+        if (root.numKeys == 2 * degree - 1) {
+            Node<T> newRoot = new Node<>(null);
+            newRoot.isLeaf = false;
+            newRoot.childPointers[0] = root;
+            splitChild(newRoot, 0);
+            root = newRoot;
+            height++;
+            insertInNodeWithSpace(root, obj);
+        } else {
+            insertInNodeWithSpace(cursor, obj);
+        }
+        size++;
+    }
 
+    /**
+     * Inserts key into the node with space
+     *
+     * @param node the B-tree node that currently has fewer than 2t-1
+     * @param key  the  to insert
+     */
+    private void insertInNodeWithSpace(Node<T> node, TreeObject key) {
+        if (node.isLeaf) {
+            int position = Arrays.binarySearch(node.keys, 0, node.numKeys, key);
+            position = -position - 1;
+            System.arraycopy(node.keys, position, node.keys, position + 1, node.numKeys - position);
+            node.keys[position] = key;
+            node.numKeys++;
+            return;
+        }
+        int position = Arrays.binarySearch(node.keys, 0, node.numKeys, key);
+        position = -position - 1;
+        if (node.childPointers[position].numKeys == 2 * degree - 1) {
+            splitChild(node, position);
+            if (key.compareTo(node.keys[position]) > 0){
+                position++;
+            }
+        }
+        insertInNodeWithSpace(node.childPointers[position], key);
+    }
+
+    /**
+     * Splits the full child at parent.childPointers[childIndex] and promotes the median key in the parent
+     *
+     * @param parent the node whose child is being split
+     * @param childIndex index of the full child within parent.childPointers
+     */
+    private void splitChild(Node<T> parent, int childIndex) {
+        Node<T> fullChild  = parent.childPointers[childIndex];
+        Node<T> newSibling = new Node<>(null);
+        newSibling.isLeaf  = fullChild.isLeaf;
+        newSibling.numKeys = degree - 1;
+        System.arraycopy(fullChild.keys, degree, newSibling.keys, 0, degree - 1);
+        if (!fullChild.isLeaf) {
+            System.arraycopy(fullChild.childPointers, degree, newSibling.childPointers, 0, degree);
+        }
+        fullChild.numKeys = degree - 1;
+        System.arraycopy(parent.childPointers, childIndex + 1, parent.childPointers,childIndex + 2, parent.numKeys - childIndex);
+        parent.childPointers[childIndex + 1] = newSibling;
+        System.arraycopy(parent.keys, childIndex, parent.keys,childIndex + 1, parent.numKeys - childIndex);
+        parent.keys[childIndex] = fullChild.keys[degree - 1];
+        parent.numKeys++;
     }
 
     /**
